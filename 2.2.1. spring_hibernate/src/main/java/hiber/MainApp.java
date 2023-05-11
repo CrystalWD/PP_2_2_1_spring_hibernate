8969019c11b9a4e9e72a56b46255be485f5a6d09
CrystalWD <egixx@mail.ru>
2023-05-11 14:20:52 +0300

    create car one-to-one getUserByModelSeries with HQL

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -19,13 +20,35 @@ public class MainApp {
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("Doge", 112)));
+      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("Supra", 113)));
+      userService.add(new User("User7", "Lastname7", "user7@mail.ru", new Car("Lowrider", 114)));
+      userService.add(new User("User8", "Lastname8", "user8@mail.ru", new Car("Toyota", 115)));
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
+
+//      List<User> users = userService.listUsers();
+//      for (User user : users) {
+//         System.out.println("Id = "+user.getId());
+//         System.out.println("First Name = "+user.getFirstName());
+//         System.out.println("Last Name = "+user.getLastName());
+//         System.out.println("Email = "+user.getEmail());
+//         if (user.getCar() != null) {
+//            System.out.println("Car = " + user.getCar().getModel() +
+//                    " SN_" + user.getCar().getSeries());
+//         }
+//         System.out.println();
+//      }
+
+      List<User> usersByCar = userService.getUsersByModelSeries("Toyota",115);
+      for (User user : usersByCar) {
          System.out.println("Id = "+user.getId());
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         if (user.getCar() != null) {
+            System.out.println("Car = " + user.getCar().getModel() +
+                    " SN_" + user.getCar().getSeries());
+         }
          System.out.println();
       }
 
